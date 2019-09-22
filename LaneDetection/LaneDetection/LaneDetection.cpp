6d7297132b36788cf90bd74fc50219f1076c999b
mazitov.ta <mazitov.ta@yandex.ru>
2019-09-22 12:08:53 +0400

    Init LaneDetection

---
+++ LaneDetection/LaneDetection/LaneDetection.cpp
@@ -0,0 +1,14 @@
+﻿
+
+#include <iostream>
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
